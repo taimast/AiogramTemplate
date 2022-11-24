c71981e3d6c4da391565614dbcb57e6714548973
taimast <81189712+taimast@users.noreply.github.com>
2022-11-24 18:43:32 +0300

    update merchants
    yookassa
    crypto cloud

--- project/crying/config/merchant/base.py
+++ project/crying/config/merchant/base.py
@@ -1,8 +1,11 @@
 import abc
 import zoneinfo
 from abc import ABC
+from functools import lru_cache
+from pprint import pprint
 from typing import Optional, Any
 
+from aiohttp import ClientSession
 from pydantic import BaseModel, SecretStr
 
 # seconds
@@ -19,6 +22,28 @@ class Merchant(BaseModel, ABC):
     api_key: SecretStr
     create_url: Optional[str] = None
     status_url: Optional[str] = None
+    session: Optional[ClientSession] = None
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    @property
+    def headers(self) -> dict:
+        return {}
+
+    async def get_session(self):
+        if self.session is None or self.session.closed:
+            self.session = ClientSession(headers=self.headers)
+        return self.session
+
+    async def close_session(self):
+        if self.session is not None:
+            await self.session.close()
+
+    async def make_request(self, method: str, url: str, **kwargs) -> Any:
+        session = await self.get_session()
+        async with session.request(method, url, **kwargs) as res:
+            return await res.json()
 
     @abc.abstractmethod
     async def create_invoice(self, amount: int | float | str) -> Any:
@@ -27,4 +52,3 @@ class Merchant(BaseModel, ABC):
     @abc.abstractmethod
     async def is_paid(self, invoice_id: str) -> bool:
         pass
-
--- project/crying/config/probe.py
+++ project/crying/config/probe.py
@@ -1,9 +1,14 @@
+import asyncio
+from asyncio import TaskGroup
 from enum import Enum
+from pprint import pprint
 
 from loguru import logger
 from pydantic import BaseModel
 
 from project.crying.config.logg_settings import init_logging
+from project.crying.config.merchant.crypto_cloud import CryptoCloud, CryptoInvoiceRequest
+from project.crying.config.merchant.yookassa import Yookassa
 
 
 class Level(str, Enum):
@@ -43,5 +48,57 @@ def main():
     long_name_for_function()
     some_long_name()
 
+async def main2():
+    c = CryptoCloud(api_key=1234, _session=None, shop_id=1234)
+    pprint(c.dict())
+    await c.get_session()
+    # await c.close_session()
+async def main4():
+    c = CryptoInvoiceRequest(amount=1, currency="RUB",  order_id="1234")
+    pprint(c.dict())
+    # await c.close_session()
+async def main3():
+    # c = Yookassa(shop_id="SrL65kqO7lp3bhCt",
+    #                 api_key="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NDYyLCJleHAiOjg4MDY5MjA5MzIwfQ.13ti0EjS-hosd4laik8JwLu0wEL2xs-UmfY-_UNTwt4")
+    # c = Yookassa(shop_id="878719",
+    #             api_key="live_Z2HLXihLcAAnjJKU38bWVrGe0vrHI1VRtdfFyuQtBqI")
+    c = Yookassa(shop_id="886879",
+                api_key="test_cmG-rdm0Azm1W2iDVE8fImHvJpO0npF-KF3qnznWmqI")
+    # c = Yookassa(shop_id="886927",
+    #             api_key="test_h28sgGpdKWLkZ-2U4IoBR0ovo_BFA8scoAzys3pC8DM")
+
+    # c2 = Yookassa(shop_id="878719",
+    #             api_key="live_Z2HLXihLcAAnjJKU38bWVrGe0vrHI1VRtdfFyuQtBqI")
+
+    # res = await c.create_invoice(amount=1, currency="RUB", order_id="1234")
+
+    tasks = []
+    for i in range(10):
+        tasks.append(c.create_invoice(amount=1, currency="RUB"))
+            # await tg.spawn(c2.create_invoice, amount=1, currency="RUB", order_id="1234")
+    invoices = await asyncio.gather(*tasks)
+    invoices_ids = [i.id for i in invoices]
+    pprint(len(invoices_ids))
+    pprint(len(set(invoices_ids)))
+
+    # await c.close_session()
+    # return
+    while True:
+        for i in invoices:
+            pprint(await c.get_invoice(i.id))
+        await asyncio.sleep(2)
+    # c = CryptoInvoiceRequest(amount=1, currency="RUB",  order_id="1234")
+    # pprint(c.dict())
+    # await c.close_session()
+
+
+async def main5():
+    c = CryptoCloud(shop_id="SrL65kqO7lp3bhCt",
+                    api_key="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NDYyLCJleHAiOjg4MDY5MjA5MzIwfQ.13ti0EjS-hosd4laik8JwLu0wEL2xs-UmfY-_UNTwt4")
+    res =  await c.create_invoice(amount=1, currency="RUB", order_id="1234")
+    pprint(res)
+
+
 if __name__ == '__main__':
-    main()
+    # main2()
+    asyncio.run(main3())
