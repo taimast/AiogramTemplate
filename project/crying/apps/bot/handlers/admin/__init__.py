abdc09eeefebd096f35077e4150d5ebd97497f3e
taimast <81189712+taimast@users.noreply.github.com>
2022-12-02 20:53:40 +0300

    minor fix

--- project/crying/apps/bot/handlers/admin/__init__.py
+++ project/crying/apps/bot/handlers/admin/__init__.py
@@ -1,7 +1,7 @@
 from aiogram import Dispatcher, Router, F
 
 from project.crying.config.config import Settings
-from .admin_menu import register_admin
+from . import menu
 
 router = Router()
 
@@ -9,5 +9,5 @@ router = Router()
 def register_admin_handlers(dp: Dispatcher, admins: list[int]):
     router.message.filter(F.from_user.id.in_(admins))
     router.callback_query.filter(F.from_user.id.in_(admins))
-    register_admin(router)
+    router.include_router(menu.router)
     dp.include_router(router)
