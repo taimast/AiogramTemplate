b3e1305a44b4c15bc0db7ea905c12b44d6036983
taimast <charlskenno@gmail.com>
2023-04-25 16:38:58 +0300

    add paginator

--- project/crying/apps/bot/callback_data/paginator.py
+++ project/crying/apps/bot/callback_data/paginator.py
@@ -42,11 +42,11 @@ class PaginatorCallback(CallbackData, prefix="paginator"):
     def switch_to_first(self) -> Self:
         return self.switch_to(1)
 
-    def has_next(self, length: int) -> bool:
-        return self.offset + self.limit < length
+    def has_next(self, length: int, page: int = 0) -> bool:
+        return self.offset + self.limit < length - page
 
-    def has_prev(self) -> bool:
-        return self.offset > 0
+    def has_prev(self, page: int = 0) -> bool:
+        return self.offset > page
 
     def slice(self, items: Sequence[T]) -> Sequence[T]:
         return items[self.offset:self.offset + self.limit]
@@ -57,32 +57,28 @@ class PaginatorCallback(CallbackData, prefix="paginator"):
         return sorted(items, key=key, reverse=self.sort_order == SortOrder.DESC)
 
     def add_pagination_buttons(self, builder: InlineKeyboardBuilder, length: int):
+        has5prev_cd = self.switch_to(self.offset - 4 * self.limit).pack() if self.has_prev(
+            4) else self.switch_to_first().pack()
+        has5next_cd = self.switch_to(self.offset + 6 * self.limit).pack() if self.has_next(
+            length, 5) else self.switch_to_last(length).pack()
+        has1prev_cd = self.prev().pack() if self.has_prev() else self.switch_to_last(length).pack()
+        has1next_cd = self.next().pack() if self.has_next(length) else self.switch_to_first().pack()
         builder.row(
             # В самое начало
-            IKButton(
-                text="≺≺",
-                callback_data=self.switch_to_first().pack()
-            ),
-
-            IKButton(
-                text="≺",
-                # if not has prev set last page
-                callback_data=self.prev().pack() if self.has_prev() else self.switch_to_last(length).pack()
-            ),
-            IKButton(
-                text=f"{self.offset // self.limit + 1}/{(length - 1) // self.limit + 1}",
-                callback_data="None"
-            ),
-            IKButton(
-                text="≻",
-                callback_data=self.next().pack() if self.has_next(length) else self.switch_to_first().pack()
-            ),
+            IKButton(text="≺≺", callback_data=self.switch_to_first().pack()),
+            # Назад на 5 страниц
+            IKButton(text="≺5", callback_data=has5prev_cd),
+            # Назад на 1 страницу
+            IKButton(text="≺", callback_data=has1prev_cd),
+            # Вперед на 1 страницу
+            IKButton(text="≻", callback_data=has1next_cd),
+            # по 5 страниц вперед
+            IKButton(text="≻5", callback_data=has5next_cd),
             # В самый конец
-            IKButton(
-                text="≻≻",
-                callback_data=self.switch_to_last(length).pack()
-            )
+            IKButton(text="≻≻", callback_data=self.switch_to_last(length).pack())
         )
+        counter_str = f"{self.offset // self.limit + 1}/{(length - 1) // self.limit + 1}"
+        builder.row(IKButton(text=counter_str, callback_data="None"))
 
     # Кнопки сортировки по убыванию и возрастанию
     def add_sort_buttons(self, builder: InlineKeyboardBuilder):
