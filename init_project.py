0ad2292481008af10c6d90f5c0eb2b59126e6f22
taimast <charlskenno@gmail.com>
2023-06-27 17:44:34 +0300

    * add ignore merchants
    * minor fix

--- init_project.py
+++ init_project.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import re
 import shutil
 import subprocess
 from distutils import dir_util
@@ -12,24 +13,16 @@ def parse_args():
     parser = argparse.ArgumentParser(description="config_file")
     parser.add_argument("-p", "--project", type=str)
     parser.add_argument("-d", "--dependencies", action="store_true", default=False)
+    parser.add_argument("-im", "--ignore-merchant", action="store_true", default=False)
     args = parser.parse_args()
-    return args.project, args.dependencies
+    return args.project, args.dependencies, args.ignore_merchant
 
 
 def get_project_dir(project_path):
     for item in project_path.iterdir():
-        if item.is_dir() and item.name not in ("tests",
-                                               ".idea",
-                                               "scripts",
-                                               "logs",
-                                               "media",
-                                               "static",
-                                               "templates",
-                                               "venv",
-                                               "__pycache__",
-                                               "backup",
-                                               ".git"):
-            return item
+        if item.is_dir():
+            if project_path.name.lower().replace("-", "_") in item.name.lower().replace("-", "_"):
+                return item
 
 
 def create_setting_files(project_path: Path, project_name: str):
@@ -47,23 +40,40 @@ def create_setting_files(project_path: Path, project_name: str):
         file_path.write_text(data, encoding="utf-8")
 
 
-def set_project_name_in_files(workdir: Path, project_name: str):
+def set_project_name_in_files(workdir: Path, project_name: str, ignore_merchant: bool = False):
     for elem in workdir.iterdir():
         if elem.is_dir():
+            if ignore_merchant:
+                if elem.name in ("merchant", "invoice"):
+                    shutil.rmtree(elem)
+                    print(f"❌ {elem} (ignore merchant)")
+                    continue
+
             if elem.name not in ("__pycache__",):
-                set_project_name_in_files(elem, project_name)
+                set_project_name_in_files(elem, project_name, ignore_merchant)
             else:
                 print(f"❌ {elem}")
         else:
-            if elem.suffix != ".pyc" and "dev" not in elem.name and "probe" not in elem.name:
+            if (elem.suffix != ".pyc") and ("dev" not in elem.name) and ("probe" not in elem.name):
                 data = elem.read_text(encoding="utf-8")
                 data = data.replace("project/crying", project_name) \
                     .replace('project.crying', project_name) \
                     .replace('crying', project_name)
                 print(f"✅ {elem}")
+
+                if ignore_merchant:
+                    data = clean_module_imports(data)
+
                 elem.write_text(data, encoding="utf-8")
             else:
                 print(f"❌ {elem}")
+
+
+def clean_module_imports(data: str, keys: list[str] = ["merchant", "invoice"]):
+    for key in keys:
+        pattern = rf'^\s*from[^\n]*\.?{key}[^\n]*import[^\n]*\(([^\n]*\n)*?\)[^\n]*\n|^\s*from[^\n]*\.?{key}[^\n]*import[^\n]*\n'
+        data = re.sub(pattern, '', data, flags=re.MULTILINE)
+    return data
 
 
 def install_dependencies(project_path: Path):
@@ -93,9 +103,10 @@ def install_dependencies(project_path: Path):
               f"poetry add {utils} --group dev"
               )
 
+
 def main():
     # subprocess.Popen(['poetry', 'show', '--tree'])
-    project_path, dependencies = parse_args()
+    project_path, dependencies, ignore_merchant = parse_args()
     if not project_path:
         permission = input("Путь до проекта не указан, установить в текущую директорию? [y/n]: ")
         if permission == "y":
@@ -112,7 +123,7 @@ def main():
         else:
             exit("❌ Проект не создан. Укажите путь до проекта через аргумент -p")
 
-    print(f"Настройка проекта {project_path}.")
+    print(f"Настройка проекта {project_path}. Ignore Merchant: {ignore_merchant}")
     workdir: Path = get_project_dir(project_path)
     project_name = workdir.name
     print(workdir)
@@ -123,7 +134,7 @@ def main():
     dir_util.copy_tree(str(TEMPLATE_DIR / "scripts"), str(project_path / "scripts"))
     dir_util.copy_tree(str(TEMPLATE_DIR / ".github"), str(project_path / ".github"))
 
-    set_project_name_in_files(workdir, project_name)
+    set_project_name_in_files(workdir, project_name, ignore_merchant)
     set_project_name_in_files(project_path / "scripts", project_name)
     set_project_name_in_files(project_path / ".github", project_path.name)
 
@@ -132,7 +143,6 @@ def main():
     if dependencies:
         print(f"Установка базовых зависимостей...")
         install_dependencies(project_path)
-
 
 
 if __name__ == "__main__":
