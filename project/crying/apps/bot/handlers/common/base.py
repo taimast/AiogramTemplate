d4a20402d6f9b629924cd3b94bb5d4dbc55da55b
taimast <charlskenno@gmail.com>
2023-10-04 18:54:23 +0300

    add deep_start

--- project/crying/apps/bot/handlers/common/base.py
+++ project/crying/apps/bot/handlers/common/base.py
@@ -2,19 +2,35 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from aiogram import Router, types, F
-from aiogram.filters import Command
+from aiogram import Router, types, F, Bot
+from aiogram.filters import Command, CommandObject, CommandStart
 from aiogram.fsm.context import FSMContext
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from project.crying.apps.bot.commands.bot_commands import BaseCommands
 from project.crying.apps.bot.keyboards.common import common_kbs
+from project.crying.db.models import User
 
 if TYPE_CHECKING:
     from project.crying.locales.stubs.ru.stub import TranslatorRunner
 
 router = Router()
 
+@router.message(CommandStart(deep_link=True))
+async def deep_start(
+        message: types.Message,
+        bot: Bot,
+        command: CommandObject,
+        session: AsyncSession,
+        user: User,
+        l10n: TranslatorRunner,
+        state: FSMContext
+):
+    """ Deep link start handler """
+    referrer_id = int(command.args)
+    if user.set_referrer(referrer_id):
+        await session.commit()
+    await start(message, l10n, bot, state)
 
 @router.message(Command(BaseCommands.START))
 @router.message(F.text.startswith("«"))
