c93790ad38637ad40bd77eb4f4b4bbb4bc289c36
taimast <charlskenno@gmail.com>
2024-01-08 21:14:52 +0300

    clean code

--- helpers/apps/account_dispatcher/manager.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass, field
-
-from .dispatcher import Dispatcher
-
-
-@dataclass
-class DispatcherManager:
-    dispatchers: dict[int, Dispatcher] = field(default_factory=dict)
