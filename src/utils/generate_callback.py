6ef508c3d7c7357703827e0e1eb052b6d6d65718
taimast <charlskenno@gmail.com>
2024-02-12 14:47:22 +0300

    update callback gen

--- src/utils/generate_callback.py
+++ src/utils/generate_callback.py
@@ -1,10 +1,12 @@
 import inspect
+import os
+import sys
+from dataclasses import dataclass
 from enum import StrEnum
 from pathlib import Path
-
-from src.apps.bot.callback_data.actions import (
-    AdminAction,
-)
+from pprint import pprint
+from src.apps.bot.callback_data.actions import *
+from src.config import BASE_DIR
 
 imports = """
 from aiogram import F
@@ -12,12 +14,20 @@ from aiogram.filters.callback_data import CallbackData
 from enum import StrEnum
 from .mixins import ActionMixin
 """
-
+@dataclass
+class GenCode:
+    name: str
+    code: str
 
 def generate_class_code(*action_enums: type[StrEnum], gen_action: bool = False):
     template = f"""
-{imports}
-"""
+{imports}"""
+
+    for enum in action_enums:
+        base_class_name = enum.__name__
+        template += f"""from .actions import {base_class_name}"""
+    template += "\n\n\n"
+
     for enum in action_enums:
         base_class_name = enum.__name__
         methods = []
@@ -48,8 +58,30 @@ class {prefix.title()}Callback(ActionMixin, CallbackData, prefix="{prefix}"):
     return template.strip() + "\n"
 
 
-generated_code = generate_class_code(
-    AdminAction
-)
-path = r"G:\CodeProjects\PycharmProjects\AiogramProjectTemplate\project\src\apps\bot\callback_data\admin.py"
-Path(path).write_text(generated_code, "utf-8")
+# generated_code = generate_class_code(
+#     AdminAction
+# )
+# path = r"G:\CodeProjects\PycharmProjects\AiogramProjectTemplate\project\src\apps\bot\callback_data\admin.py"
+# Path(path).write_text(generated_code, "utf-8")
+
+def generate_all_callbacks():
+    # Получаем список всех actions из файла actions.py
+    actions = [obj for name, obj in inspect.getmembers(sys.modules[__name__]) if inspect.isclass(obj) and issubclass(obj, StrEnum)]
+    # filter out all Action classes
+    actions = [action for action in actions if "Action" in action.__name__]
+
+    # Для каждого action генерируем Callback и сохраняем в новый модуль
+    for action in actions:
+        module_name = action.__name__.replace('Action', '').lower()
+        callback_path = BASE_DIR / f"src/apps/bot/callback_data/{module_name}.py"
+        # if callback_path.exists():
+        #     print(f"Callback module {module_name} already exists")
+        #     continue
+        callback_code = generate_class_code(action, gen_action=False)
+        callback_path.write_text(callback_code, "utf-8")
+
+def main():
+    generate_all_callbacks()
+
+if __name__ == '__main__':
+    main()
