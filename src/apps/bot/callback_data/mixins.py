17f8e97fb842e4bd3e98c0f1f1a8aac6094a21a0
taimast <charlskenno@gmail.com>
2024-09-14 17:29:47 +0300

    refactor: Update filter methods to return CallbackQueryFilter instead of Self

--- src/apps/bot/callback_data/mixins.py
+++ src/apps/bot/callback_data/mixins.py
@@ -2,7 +2,7 @@ from enum import StrEnum
 from typing import Self
 
 from aiogram import F
-from aiogram.filters.callback_data import CallbackData
+from aiogram.filters.callback_data import CallbackData, CallbackQueryFilter
 
 from .actions import Action
 
@@ -11,39 +11,41 @@ class FilterActionMixin(CallbackData, prefix="filter_action_mixin"):
     action: Action
 
     @classmethod
-    def filter_create(cls) -> Self:
+    def filter_create(cls) -> CallbackQueryFilter:
         return cls.filter(F.action == Action.CREATE)
 
     @classmethod
-    def filter_all(cls) -> Self:
+    def filter_all(cls) -> CallbackQueryFilter:
         return cls.filter(F.action == Action.ALL)
 
     @classmethod
-    def filter_delete(cls) -> Self:
+    def filter_delete(cls) -> CallbackQueryFilter:
         return cls.filter(F.action == Action.DELETE)
 
     @classmethod
-    def filter_get(cls) -> Self:
+    def filter_get(cls) -> CallbackQueryFilter:
         return cls.filter(F.action == Action.GET)
 
     @classmethod
-    def filter_with_action(cls, action: StrEnum) -> Self:
+    def filter_with_action(cls, action: StrEnum) -> CallbackQueryFilter:
         return cls.filter(F.action == action)
+
 
 class ActionCreateMixin(CallbackData, prefix="action_copy_mixin"):
     action: Action
 
-    def get(self, id: int) -> Self:
-        return self.model_copy(update={'action': Action.GET, 'id': id})
+    def get(self, id: int | None = None) -> Self:
+        id = id or self.id
+        return self.model_copy(update={"action": Action.GET, "id": id})
 
     def all(self) -> Self:
-        return self.model_copy(update={'action': Action.ALL})
+        return self.model_copy(update={"action": Action.ALL})
 
     def create(self) -> Self:
-        return self.model_copy(update={'action': Action.CREATE})
+        return self.model_copy(update={"action": Action.CREATE})
 
     def delete(self) -> Self:
-        return self.model_copy(update={'action': Action.DELETE})
+        return self.model_copy(update={"action": Action.DELETE})
 
     @classmethod
     def new_create(cls, id: int = None) -> Self:
@@ -64,6 +66,7 @@ class ActionCreateMixin(CallbackData, prefix="action_copy_mixin"):
     @classmethod
     def with_action(cls, action: StrEnum, **kwargs) -> Self:
         return cls(action=action, **kwargs)
+
 
 # Общий класс для миксинов
 class ActionMixin(ActionCreateMixin, FilterActionMixin, CallbackData, prefix="action_mixin"):
