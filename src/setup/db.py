c7d7dc28615a3747e4525baa48d736749a209fe6
taimast <charlskenno@gmail.com>
2024-11-16 10:56:44 +0300

    refactor(db, webhook): simplify init_db, update webhook settings usage

--- src/setup/db.py
+++ src/setup/db.py
@@ -13,26 +13,7 @@ async def close_db():
     logger.info("Database closed")
 
 
-async def dev_init_db(db: Database = PostgresDB.default()) -> async_sessionmaker:
-    from sqlalchemy_utils import database_exists, create_database
-    logger.info(f"Initializing Database {db.database}[{db.host}]...")
-    engine = create_async_engine(db.url, echo=True)
-
-    if not database_exists(db.sync_url):
-        create_database(db.sync_url)
-        logger.info(f"Database {db.database}[{db.host}] created")
-
-    async with engine.begin() as conn:
-        # await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
-
-    logger.info(f"Database {db.database}[{db.host}] initialized")
-    return async_sessionmaker(engine, expire_on_commit=False)
-
-
-async def init_db(db: Database = PostgresDB.default(), dev: bool = False) -> async_sessionmaker:
-    if dev:
-        return await dev_init_db(db)
+async def init_db(db: Database = PostgresDB.default()) -> async_sessionmaker:
     logger.info(f"Initializing {db}...")
     engine = create_async_engine(db.url)
 
--- src/setup/webhook.py
+++ src/setup/webhook.py
@@ -2,31 +2,47 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from aiogram.client.default import DefaultBotProperties
+from aiogram.webhook.aiohttp_server import (
+    SimpleRequestHandler,
+    TokenBasedRequestHandler,
+    setup_application,
+)
 from aiohttp import web
 from loguru import logger
+from pprint import pformat
+from src.config import WebhookSettings
 
 from ..config import Settings
 
 
-async def start_webhook(bot: Bot, dp: Dispatcher, settings: Settings):
+async def start_webhook(
+    bot: Bot,
+    dp: Dispatcher,
+    webhook: WebhookSettings,
+):
     """Start webhook."""
     # Disable aiohttp logs
     logging.getLogger("aiohttp").setLevel(logging.WARNING)
 
     # Set webhook
+    certificate = webhook.get_certfile()
     await bot.set_webhook(
-        certificate=settings.webhook.get_certfile(),
-        url=settings.webhook.url,
+        certificate=certificate,
+        url=webhook.path.format(bot_token=bot.token),
         drop_pending_updates=True,
         allowed_updates=dp.resolve_used_update_types(),
     )
+    logger.info(f"Set webhook to {webhook.url}")
 
     # Create aiohttp application
     app = web.Application()
-    SimpleRequestHandler(dispatcher=dp, bot=bot).register(
-        app, path=settings.webhook.path
-    )
+
+    bot_settings = {"default": bot.default, "session": bot.session}
+    TokenBasedRequestHandler(
+        dispatcher=dp,
+        bot_settings=bot_settings,
+    ).register(app, path=webhook.path)
 
     # Configure startup and shutdown processes
     setup_application(app, dp)
@@ -37,13 +53,13 @@ async def start_webhook(bot: Bot, dp: Dispatcher, settings: Settings):
 
     site = web.TCPSite(
         runner,
-        host=settings.webhook.host,
-        port=settings.webhook.port,
-        ssl_context=settings.webhook.get_ssl_context(),
+        host=webhook.host,
+        port=webhook.port,
+        ssl_context=webhook.get_ssl_context(),
     )
     # web.run_app(app, host=config.webhook.host, port=config.webhook.port)
     await site.start()
 
-    logger.info("Webhook started")
+    logger.info(f"Webhook started at {webhook.host}:{webhook.port}{webhook.path}")
     # Run forever
     await asyncio.Future()
