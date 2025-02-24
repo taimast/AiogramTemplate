ae942c081a6e2c0b2bd0c4ec0c26947f6d999bac
taimast <charlskenno@gmail.com>
2025-02-25 00:59:43 +0300

    feat(types): extend message and callback query classes with additional attributes

--- src/apps/bot/types/message.py
+++ src/apps/bot/types/message.py
@@ -1,5 +1,25 @@
 from aiogram import Bot
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import BusinessConnection, CallbackQuery, InlineQuery, Message
+
+
+class FilledMessage(Message):
+    text: str  # type: ignore
+    bot: Bot  # type: ignore
+    business_connection_id: str  # type: ignore
+
+
+class FilledCallbackQuery(CallbackQuery):
+    data: str  # type: ignore
+    bot: Bot  # type: ignore
+    message: Message  # type: ignore
+
+
+class FilledBusinessConnection(BusinessConnection):
+    bot: Bot  # type: ignore
+
+
+class FilledInlineQuery(InlineQuery):
+    bot: Bot  # type: ignore
 
 
 class NonEmptyBotMessage(Message):
